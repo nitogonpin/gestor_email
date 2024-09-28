0f53ab2e10d51e0365ddc250cbd08349c1c9b215
nito1971 <bgonpin@gmail.com>
2024-09-28 15:47:40 +0200

    añadido contenido

--- utilleria/extraer_direccion_correo.py
+++ utilleria/extraer_direccion_correo.py
@@ -1,6 +1,23 @@
 import pymongo
 import email
 
+correos_ya_recopilados = set()
+
+def listaDeCorreosYaRecopilados(direccion_de_correo):
+    lista = []
+    try:
+        client = pymongo.MongoClient("mongodb://localhost:2018")
+        db = client["direcciones_recopiladas"]
+        collection = db[direccion_de_correo]
+        correos = collection.find()
+        for i in correos:
+            lista.append(i["_id"])
+            #print(i["_id"])
+        return lista
+    except Exception as e:
+        print("Error al obtener la lista de correos ya recopilados: ", e)
+
+
 def recuperarDaseDatosMongoDB(direccion):
     """
     Recupera los correos electrnicos guardados en la base de datos MongoDB
@@ -38,6 +55,34 @@ def recuperarDaseDatosMongoDB(direccion):
     # Devuelve la lista de direcciones de correo electr nico
     return lista_direcciones
 
+def insertarDireccionCorreoMongoDB(correo_recopilado, cuenta_correo):
+    """
+    Inserta una direcci n de correo electr nico en la base de datos MongoDB
+
+    :param direccion: La direcci n de correo electr nico
+    :type direccion: str
+    :param cuenta_correo: La cuenta de correo de Gmail
+    :type cuenta_correo: str
+    """
+    # Conecta a la base de datos MongoDB
+    # La variable "client" es la conexi n a la base de datos
+    try:
+        client = pymongo.MongoClient("mongodb://localhost:2018")
+
+        # Selecciona la base de datos "correos"
+        # La variable "db" es la base de datos seleccionada
+        db = client["direcciones_recopiladas"]
+
+        # Selecciona la colecci n que se indica en la variable "cuenta_correo"
+        # La variable "collection" es la colecci n seleccionada
+        collection = db[cuenta_correo]
+
+        # Inserta la direcci n de correo electr nico en la colecci n
+        # La variable "direccion" es la direcci n de correo electr nico que se va a insertar
+        collection.insert_one({"_id": correo_recopilado})
+    except Exception as e:
+        print(e)
+
 def limpiar_direccion(direccion):
     """
     Esta funci n toma una cadena de texto que representa una direcci n
@@ -70,36 +115,46 @@ def limpiar_direccion(direccion):
 
 
 def main():
-    # Pide al usuario que introduzca su correo electr nico
+    # Pide al usuario que introduzca su correo electrónico
+    # La variable "direccion" es la cadena que se introduce
     direccion = input("Introduce tu correo: ")
 
-    # Recupera los correos electr nicos de la base de datos MongoDB
-    # que se encuentran en la carpeta del usuario
+    # Recupera la lista de correos electrónicos ya recopilados
+    # para la cuenta de correo especificada
+    # La variable "correos_ya_recopilados" es la lista de correos electrónicos ya recopilados
+    correos_ya_recopilados = listaDeCorreosYaRecopilados(direccion)
+
+    # Recupera la lista de direcciones de correo electrónico
+    # guardadas en la base de datos MongoDB
+    # La variable "lista_direcciones" es la lista de direcciones de correo electrónico
     lista_direcciones = recuperarDaseDatosMongoDB(direccion)
 
-    # Crea un conjunto vac o para almacenar las direcciones de correo
-    # electr nico limpias
+    # Crea una lista para guardar las direcciones de correo electr nico
+    # sin etiquetas HTML
+    # La variable "lista_direcciones_limpia" es la lista vacía
     lista_direcciones_limpia = set()
 
-    # Itera sobre la lista de direcciones de correo electr nico
-    # y las limpia de etiquetas HTML
+    # Itera sobre la lista de direcciones de correo electrónico
+    # y limpia cada dirección de correo electr nico
+    # La variable "linea_a_limpiar" es la cadena con la dirección de correo electrónico limpia
     for i in lista_direcciones:
-        # Limpia la direcci n de correo electr nico
-        # de etiquetas HTML
         linea_a_limpiar = limpiar_direccion(i)
-
-        # Agrega la direcci n limpia al conjunto
         lista_direcciones_limpia.add(linea_a_limpiar)
 
-    # Imprime las direcciones de correo electr nico limpias
-    for i in lista_direcciones_limpia:
-        print(i)
+    # Itera sobre la lista de direcciones de correo electrónico limpias
+    # y verifica si cada dirección de correo electrónico ya ha sido recopilada
+    # Si no ha sido recopilada, la agrega a la lista de correos electrónicos ya recopilados
+    for correo in lista_direcciones_limpia:
+        #print(correo)
+        if correo not in correos_ya_recopilados:
+            insertarDireccionCorreoMongoDB(correo, direccion)
+            correos_ya_recopilados.append(correo)
+            print(correo)
 
-    # Imprime el n mero de correos electr nicos recuperados
+    # Imprime el número de correos electrónicos recopilados
     print( "*" * 24)
     print("Recuperados: ", len(lista_direcciones_limpia), " correos.")
     print( "*" * 24)
-
 
 if __name__ == "__main__":
     main()
